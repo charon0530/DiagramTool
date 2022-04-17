55bad08168ee8ae2575755fa7642c1ffe74223a8
charon0530 <45917075+charon0530@users.noreply.github.com>
2022-04-17 18:52:04 +0900

    CreateAndDraggable
    Can create elements.
    Can drag elements.

--- js/diagram.js
+++ js/diagram.js
@@ -2,10 +2,19 @@ const handler = document.querySelector(".handler");
 const wrapper = document.querySelector(".contents");
 const left_region = document.querySelector(".left_region");
 const menus = document.querySelectorAll(".menu");
+const pad_items = document.querySelectorAll(".menu_item svg");
+const board = document.querySelector(".mid_region");
 const MIN_WIDTH = 0;
 
+//Drag Node
+let selectedElement = null;
+let selectedOffset = {};
+///////////////////////////
+
+//Resizing left_region
 let canResizing = false;
 let clicked_Xpos = 0;
+//////////////////////////
 document.addEventListener("mousedown", (event) => {
     if (event.target !== handler) return;
     clicked_Xpos = event.clientX - handler.getBoundingClientRect().left;
@@ -27,6 +36,55 @@ document.addEventListener("mouseup", (evnet) => {
 menus.forEach((menu) => {
     menu.addEventListener("click", function (event) {
         event.stopPropagation();
-        event.currentTarget.parentNode.childNodes[3].classList.toggle("active");
+        event.currentTarget.parentNode.children[1].classList.toggle("active");
     });
+});
+
+pad_items.forEach((item) => {
+    item.addEventListener("click", function (event) {
+        const element = event.currentTarget.children[0].cloneNode(true);
+        element.setAttribute("width", element.getAttribute("width") * 5);
+        element.setAttribute("height", element.getAttribute("height") * 5);
+        element.classList.add("draggable");
+        element.setAttribute("x", 0);
+        element.setAttribute("y", 0);
+
+        document.addEventListener("mousedown", onMouseDown);
+        document.addEventListener("mousemove", onMouseMove);
+        document.addEventListener("mouseup", onMouseUp);
+        document.addEventListener("mouseleave", onMouseLeave);
+
+        board.appendChild(element);
+    });
+
+    function onMouseDown(event) {
+        if (event.target.classList.contains("draggable")) {
+            selectedElement = event.target;
+            selectedOffset = _ViewPort2BoardSpace(event);
+            console.log(selectedOffset);
+            selectedOffset.x -= selectedElement.getAttribute("x");
+            selectedOffset.y -= selectedElement.getAttribute("y");
+        }
+    }
+    function onMouseMove(event) {
+        if (selectedElement !== null) {
+            event.preventDefault();
+            let coord = _ViewPort2BoardSpace(event);
+            console.log(selectedOffset.x);
+            selectedElement.setAttribute("x", coord.x - selectedOffset.x);
+            selectedElement.setAttribute("y", coord.y - selectedOffset.y);
+        }
+    }
+    function onMouseUp(event) {
+        selectedElement = null;
+    }
+    function onMouseLeave(event) {}
+    function _ViewPort2BoardSpace(event) {
+        const CTM = board.getScreenCTM();
+
+        return {
+            x: (event.clientX - CTM.e) / CTM.a,
+            y: (event.clientY - CTM.f) / CTM.d,
+        };
+    }
 });
