5832c9b71c82a04dc20df577b41976b7438de08c
charon0530 <45917075+charon0530@users.noreply.github.com>
2022-04-22 20:09:29 +0900

    Implement link
    Implement link
    fix resize

--- js/diagram.js
+++ js/diagram.js
@@ -6,6 +6,11 @@ const menus = document.querySelectorAll(".menu");
 const item_boxes = document.querySelectorAll(".menu_item svg");
 const mid_region = document.querySelector(".mid_region");
 const board = document.querySelector(".board");
+const text = document.querySelector("#text");
+const font_size = document.querySelector("#font_size");
+const link_btn = document.querySelector("#link_btn");
+const link_del_btn = document.querySelector("#link_dle_btn");
+
 const MIN_WIDTH = 0;
 
 const ns = "http://www.w3.org/2000/svg";
@@ -13,15 +18,18 @@ const LEFT_MOUSE_BUTTON = 0;
 const MIDDLE_MOUSE_BUTTON = 1;
 const RIGHT_MOUSE_BUTTON = 2;
 
-const DEFAULT_NODE_SCALE = 5;
+const DEFAULT_NODE_SCALE = 3;
 const DEFAULT_SNAP_GAP = 10;
 let ZOOM_VAL = 1;
 
-//Drag Node
+//Select Node
 let selectedElement = null;
 const selectedElement_set = new Set();
+
+//Drag Node
 let isDragging = false;
-/**********************************************************************************************/
+
+//Creating Node
 let creatingElement_g = null;
 let selectedBox_svg = null;
 
@@ -29,7 +37,16 @@ let selectedBox_svg = null;
 let canResizing = false;
 let clicked_Xpos = 0;
 
+//Resizer
 let curResizersTag = null;
+let selectedResizer = null;
+
+let isLinking = false;
+let isDelLinking = false;
+let from_node = null;
+let to_node = null;
+let from_to_list = [];
+let LeaderLine_list = [];
 /**********************************************************************************************/
 //#region Util
 let CTRLKEY = false;
@@ -88,7 +105,13 @@ function _DeleteGtag(gTag) {
     console.log("[G_TAG_DELETED]");
     selectedElement_set.delete(gTag);
     if (selectedElement === gTag) selectedElement = null;
+    showResizers(selectedElement);
     gTag.remove();
+    from_to_list = from_to_list.filter((x) => x[0] !== gTag && x[1] !== gTag);
+    isLinking = false;
+    from_node = null;
+    to_node = null;
+    drawLines();
 }
 //#endregion
 
@@ -105,14 +128,6 @@ function onMouseMove_Handler(event) {
         left_region.style.width =
             Math.max(MIN_WIDTH, pointerRelativeXpos) + "px";
         left_region.style.flexGrow = 0;
-    } else if (creatingElement_g) {
-        _MoveGtag(
-            creatingElement_g,
-            event.clientX,
-            event.clientY,
-            creatingElement_g.clickOffsetX,
-            creatingElement_g.clickOffsetY
-        );
     }
 }
 function onMouseUp_Handler(event) {
@@ -144,50 +159,138 @@ menus.forEach((menu) => {
 });
 //#endregion
 /**********************************************************************************************/
+function onMouseDown_Resizer(event) {
+    event.preventDefault();
+    selectedResizer = event.target;
+
+    selectedResizer.clickOffsetX = event.clientX;
+    selectedResizer.clickOffsetY = event.clientY;
+
+    const reg = /translate\((.*?) (.*?)\) scale\((.*?) (.*?)\)/;
+
+    const regResult = selectedElement.getAttribute("transform").match(reg);
+    const [translateX, translateY] = [regResult[1], regResult[2]];
+    const [scaleX, scaleY] = [regResult[3], regResult[4]];
+    selectedResizer.translateOffsetX = Number(translateX);
+    selectedResizer.translateOffsetY = Number(translateY);
+    selectedResizer.scaleOffsetX = Number(scaleX);
+    selectedResizer.scaleOffsetY = Number(scaleY);
+
+    selectedResizer.selectedElementRect =
+        selectedElement.getBoundingClientRect();
+}
+
+function onMouseMove_Resizer(event) {
+    if (selectedResizer) {
+        const gTag = selectedElement;
+        const board_pos_click = _ViewPort2BoardSpace(
+            event.clientX,
+            event.clientY
+        );
+        const board_pos_offset = _ViewPort2BoardSpace(
+            selectedResizer.clickOffsetX,
+            selectedResizer.clickOffsetY
+        );
+        const deltaX =
+            (board_pos_click.x - board_pos_offset.x) *
+            selectedResizer.scaleOffsetX;
+        const deltaY =
+            (board_pos_click.y - board_pos_offset.y) *
+            selectedResizer.scaleOffsetY;
+
+        //원래길이
+        const selectedElementRect = selectedResizer.selectedElementRect;
+        //zoom은 나중에처리
+
+        const ratioX = deltaX / selectedElementRect.width;
+        const ratioY = deltaY / selectedElementRect.height;
+
+        const resultX = selectedResizer.scaleOffsetX + ratioX;
+        const resultY = selectedResizer.scaleOffsetY + ratioY;
+
+        const targetBBox = gTag.querySelector(".selected").getBBox();
+        const [snapX, snapY] = _SnapBoardSpace(
+            resultX * targetBBox.width,
+            resultY * targetBBox.height
+        );
+        const X = snapX / targetBBox.width;
+        const Y = snapY / targetBBox.height;
+        _SetGtagTransform(
+            gTag,
+            selectedResizer.translateOffsetX,
+            selectedResizer.translateOffsetY,
+            X,
+            Y
+        );
+        showResizers(gTag);
+    }
+}
+
+function onMouseUp_Resizer(event) {
+    selectedResizer = null;
+}
+
 function hideReziers() {
     if (curResizersTag) {
         curResizersTag.remove();
         curResizersTag = null;
     }
 }
-function showResizers(wrapper_g) {
+function showResizers(targetElement) {
     hideReziers();
-    const element = selectedElement.querySelector(".selected");
+    if (targetElement === null) return;
+    const element = targetElement.querySelector(".selected");
     const resizers_g = document.createElementNS(ns, "g");
     resizers_g.classList.add("resizers");
 
+    const reg = /translate\((.*?) (.*?)\) scale\((.*?) (.*?)\)/;
+    const regResult = targetElement.getAttribute("transform").match(reg);
+    const [translateX, translateY] = [regResult[1], regResult[2]].map((x) =>
+        Number(x)
+    );
+    const [scaleX, scaleY] = [regResult[3], regResult[4]].map((x) => Number(x));
     const point = element.getPointAtLength(0);
-    const [offsetX, offsetY] = [point.x, point.y];
+    const [offsetX, offsetY] = [point.x * scaleX, point.y * scaleY];
     resizers_g.setAttribute(
         "transform",
-        `translate(${offsetX} ${offsetY}) scale(1 1)`
+        `translate(${offsetX + translateX} ${offsetY + translateY}) scale(1 1)`
     );
 
+    const RESIZER_SIZE = 5;
     const SVGRect = element.getBBox();
+    SVGRect.width *= scaleX;
+    SVGRect.height *= scaleY;
 
-    const resizer_T = makeResizer(SVGRect.width / 2, 0, 1);
-    const resizer_R = makeResizer(SVGRect.width, SVGRect.height / 2, 1);
-    const resizer_D = makeResizer(SVGRect.width / 2, SVGRect.height, 1);
-    const resizer_L = makeResizer(0, SVGRect.height / 2, 1);
-    //#endregion
+    const resizer_RB = makeResizer(
+        SVGRect.width,
+        SVGRect.height,
+        RESIZER_SIZE / ZOOM_VAL,
+        "resizer_RB"
+    );
 
-    resizers_g.appendChild(resizer_T);
-    resizers_g.appendChild(resizer_R);
-    resizers_g.appendChild(resizer_D);
-    resizers_g.appendChild(resizer_L);
+    resizers_g.appendChild(resizer_RB);
 
-    wrapper_g.appendChild(resizers_g);
+    resizers_g.addEventListener("mousedown", onMouseDown_Resizer);
+    document.addEventListener("mousemove", onMouseMove_Resizer);
+    document.addEventListener("mouseup", onMouseUp_Resizer);
+
+    board.appendChild(resizers_g);
     curResizersTag = resizers_g;
 }
 function selectNode(wrapper_g, event) {
     //event.target = real tag
     //element = top of g
     //임시로 구현 => 선택 시 resizers을 그린 g태그를 visible하게 바꿀것임
+    text.value = wrapper_g.querySelector("text").innerHTML;
+    font_size.value = wrapper_g.querySelector("text").getAttribute("font-size");
     selectedElement = wrapper_g;
     event.target.classList.add("selected");
     wrapper_g.clickOffsetX = event.clientX;
     wrapper_g.clickOffsetY = event.clientY;
     selectedElement_set.add(wrapper_g);
+
+    board.removeChild(selectedElement);
+    board.appendChild(selectedElement);
 
     _SetSeletedOffsets();
 }
@@ -203,14 +306,30 @@ function deSelectNodeAll() {
         deSelectNode(target);
     }
 }
+function drawLines() {
+    for (let x of LeaderLine_list) {
+        x.remove();
+    }
+    LeaderLine_list = [];
+    for (let [from, to] of from_to_list) {
+        if (from === null || to === null) continue;
+        let obj = new LeaderLine(from, to);
+        LeaderLine_list.push(obj);
+    }
+}
 
 function onMouseDown_Board(event) {
-    //console.log("[MOUSE_DOWN_EVENT_TARGET]",event.target);
+    //console.log("[MOUSE_DOWN_EVENT_TARGET]", event.target);
     if (event.button !== LEFT_MOUSE_BUTTON) return;
     if (event.target === board) {
-        //console.log("[SELECTED_ELEMENT_SET CLEAR!]");
+        console.log("[SELECTED_ELEMENT_SET CLEAR!]");
         deSelectNodeAll();
         hideReziers();
+
+        link_btn.innerHTML = "link";
+        isLinking = false;
+        from_node = null;
+        to_node = null;
     } else if (
         event.target.classList.contains("draggable") &&
         event.target.classList.contains("selectable")
@@ -226,17 +345,43 @@ function onMouseDown_Board(event) {
         } else {
             if (selectedElement_set.has(picked_g)) {
                 selectNode(picked_g, event);
-                showResizers(picked_g);
+                showResizers(selectedElement);
             } else {
                 deSelectNodeAll();
                 selectNode(picked_g, event);
-                showResizers(picked_g);
+                showResizers(selectedElement);
             }
-        }
-
-        if (selectedElement) {
-            board.removeChild(selectedElement);
-            board.appendChild(selectedElement);
+            if (isLinking) {
+                if (from_node === null) {
+                    from_node = picked_g;
+                    link_btn.innerHTML = "select to node";
+                } else {
+                    to_node = picked_g;
+                    let obj = new LeaderLine(from_node, to_node);
+                    LeaderLine_list.push(obj);
+                    from_to_list.push([from_node, to_node]);
+                    link_btn.innerHTML = "link";
+                    isLinking = false;
+                    from_node = null;
+                    to_node = null;
+                    drawLines();
+                }
+            } else if (isDelLinking) {
+                if (from_node === null) {
+                    from_node = picked_g;
+                    link_del_btn.innerHTML = "select to node";
+                } else {
+                    to_node = picked_g;
+                    from_to_list = from_to_list.filter(
+                        (x) => !(x[0] === from_node && x[1] === to_node)
+                    );
+                    link_del_btn.innerHTML = "DelLink";
+                    isDelLinking = false;
+                    from_node = null;
+                    to_node = null;
+                    drawLines();
+                }
+            }
         }
         isDragging = true;
     }
@@ -249,7 +394,6 @@ function onMouseMove_Board(event) {
         selectedElement_set.has(selectedElement)
     ) {
         event.preventDefault();
-
         const [afterX, afterY] = [event.clientX, event.clientY];
         const deltaX = (afterX - selectedElement.clickOffsetX) / ZOOM_VAL;
         const deltaY = (afterY - selectedElement.clickOffsetY) / ZOOM_VAL;
@@ -263,6 +407,7 @@ function onMouseMove_Board(event) {
         for (let target of selectedElement_set) {
             const targetX = Number(target.translateOffsetX) + Number(deltaX);
             const targetY = Number(target.translateOffsetY) + Number(deltaY);
+
             const [snapX, snapY] = _SnapBoardSpace(targetX, targetY);
             _SetGtagTransform(
                 target,
@@ -271,6 +416,10 @@ function onMouseMove_Board(event) {
                 target.scaleOffsetX,
                 target.scaleOffsetY
             );
+        }
+        drawLines();
+        if (curResizersTag) {
+            showResizers(selectedElement);
         }
     }
 }
@@ -282,7 +431,6 @@ function onMouseUp_Board(event) {
     if (creatingElement_g !== null) {
         const board_pos = _ViewPort2BoardSpace(event.clientX, event.clientY);
         const [snapX, snapY] = _SnapBoardSpace(board_pos.x, board_pos.y);
-        console.log(board_pos);
         _SetGtagTransform(
             creatingElement_g,
             snapX,
@@ -295,8 +443,13 @@ function onMouseUp_Board(event) {
     }
 }
 
-function makeResizer(cx, cy, r) {
+board.addEventListener("mousedown", onMouseDown_Board);
+board.addEventListener("mousemove", onMouseMove_Board);
+board.addEventListener("mouseup", onMouseUp_Board);
+
+function makeResizer(cx, cy, r, className = "") {
     const resizer = document.createElementNS(ns, "circle");
+    resizer.classList.add(className);
     resizer.setAttribute("cx", cx);
     resizer.setAttribute("cy", cy);
     resizer.setAttribute("r", r);
@@ -323,15 +476,21 @@ function makeNode(
     wrapper_g.scaleOffsetX = Number(size);
     wrapper_g.scaleOffsetY = Number(size);
 
-    wrapper_g.appendChild(element);
-    parent.appendChild(wrapper_g);
+    const text = document.createElementNS(ns, "text");
+    text.setAttribute("alignment-baseline", "central");
+    text.setAttribute("text-anchor", "middle");
+    text.setAttribute("font-size", 7);
 
+    wrapper_g.appendChild(element);
+    wrapper_g.appendChild(text);
+    parent.appendChild(wrapper_g);
+    text.setAttribute("x", element.getBBox().width / 2);
+    text.setAttribute("y", element.getBBox().height / 2);
     return wrapper_g;
 }
 
 function initItemBox(box) {
     function onMouseDown_ItemBox(event) {
-        console.log(event.currentTarget);
         if (event.button !== LEFT_MOUSE_BUTTON) return;
         const element = event.currentTarget
             .querySelector(".item")
@@ -348,7 +507,17 @@ function initItemBox(box) {
         creatingElement_g.clickOffsetX = event.clientX;
         creatingElement_g.clickOffsetY = event.clientY;
         selectedBox_svg = event.currentTarget;
-        console.log(selectedBox_svg);
+    }
+    function onMouseMove_ItemBox(event) {
+        if (creatingElement_g) {
+            _MoveGtag(
+                creatingElement_g,
+                event.clientX,
+                event.clientY,
+                creatingElement_g.clickOffsetX,
+                creatingElement_g.clickOffsetY
+            );
+        }
     }
 
     function onMouseUp_ItemBox(event) {
@@ -381,13 +550,10 @@ function initItemBox(box) {
     ///////////////////////////////////////////////////////////////
 
     box.addEventListener("mousedown", onMouseDown_ItemBox);
+    document.addEventListener("mousemove", onMouseMove_ItemBox);
     box.addEventListener("mouseup", onMouseUp_ItemBox);
 }
 item_boxes.forEach(initItemBox);
-
-board.addEventListener("mousedown", onMouseDown_Board);
-board.addEventListener("mousemove", onMouseMove_Board);
-board.addEventListener("mouseup", onMouseUp_Board);
 
 //grab board
 ////////////////////////////////////////////////
@@ -442,7 +608,9 @@ function onWheel_Board(event) {
             board.style.zoom = ZOOM_VAL;
         }
         _SetSeletedOffsets();
+        showResizers(selectedElement);
     }
+    drawLines();
 }
 board.addEventListener("mousewheel", onWheel_Board);
 
@@ -455,3 +623,32 @@ function onKeyDownDel(event) {
     }
 }
 document.addEventListener("keydown", onKeyDownDel);
+
+function onChangeText(event) {
+    selectedElement.querySelector("text").innerHTML = event.target.value;
+}
+text.addEventListener("change", onChangeText);
+
+function onChangeFontSize(event) {
+    selectedElement
+        .querySelector("text")
+        .setAttribute("font-size", event.target.value);
+}
+font_size.addEventListener("change", onChangeFontSize);
+
+function onClickLinkBTN(event) {
+    if (event.target.innerHTML === "link") {
+        isLinking = true;
+        event.target.innerHTML = "select from";
+    }
+}
+link_btn.addEventListener("click", onClickLinkBTN);
+
+function onClickLinkDelBTN(event) {
+    isLinking = false;
+    if (event.target.innerHTML === "DelLink") {
+        isDelLinking = true;
+        event.target.innerHTML = "select from";
+    }
+}
+link_del_btn.addEventListener("click", onClickLinkDelBTN);
